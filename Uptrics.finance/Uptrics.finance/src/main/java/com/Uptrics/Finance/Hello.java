f3c41a86ea22fe15823e54a2ddb4c6a2363abafe
ShriKrishna <shrikrushna.shiwane5@gmail.com>
2025-05-19 20:59:41 +0530

    hhhhaaaaaaaaaahhhaaaa

--- Uptrics.finance/Uptrics.finance/src/main/java/com/Uptrics/Finance/Hello.java
+++ Uptrics.finance/Uptrics.finance/src/main/java/com/Uptrics/Finance/Hello.java
@@ -3,5 +3,6 @@ package com.Uptrics.Finance;
 public class Hello {
 public static void main(String[] args) {
 	System.out.println("hello am learning git command");
+	System.out.println("doing hands on practice");
 }
 }
