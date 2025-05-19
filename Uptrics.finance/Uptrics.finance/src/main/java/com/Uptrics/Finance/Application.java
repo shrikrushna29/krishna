58513ced5c829245578852602b4e40da46ba6c6a
ShriKrishna <shrikrushna.shiwane5@gmail.com>
2025-05-19 20:48:00 +0530

    AAAA

--- Uptrics.finance/Uptrics.finance/src/main/java/com/Uptrics/Finance/Application.java
+++ Uptrics.finance/Uptrics.finance/src/main/java/com/Uptrics/Finance/Application.java
@@ -14,7 +14,7 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 		
 		
-		System.out.println("App started "+new Date()	);
+		System.out.println("App started "+new Date()	+ "AAA");
 	}
 
 }
